879eedfbf320b19bdfb33735a86060787fbfb6b3
kmm11411 <56to74@gmail.com>
2020-08-15 09:32:30 -0400

    basic linerenderer for sensemetal

--- Assets/Scripts/Movements/SenseMetalController.cs
+++ Assets/Scripts/Movements/SenseMetalController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SenseMetalController : MonoBehaviour
+public class SenseMetalController : MovementType
 {
 
     [SerializeField]
@@ -10,27 +10,26 @@ public class SenseMetalController : MonoBehaviour
 
     Color c = Color.blue;
 
-    PlayerInput playerInput;
-
     // Start is called before the first frame update
-    void Start()
-    {
+    // void Start()
+    // {
         
-    }
+    // }
 
     // Update is called once per frame
     void Update()
     {
         if(playerInput.SenseMetal()) {
-            //LineRenderer lRend = gameObject.AddComponent(typeof(LineRenderer)) as LineRenderer;
-            LineRenderer lRend = gameObject.AddComponent<LineRenderer>();
+            LineRenderer lRend = renderTarget.AddComponent<LineRenderer>();
             lRend.SetPosition(0, transform.position);
             lRend.SetPosition(1, renderTarget.transform.position);
+            lRend.startWidth = 0.05f;
+            lRend.endWidth = 0.05f;
             lRend.material = new Material(Shader.Find("Sprites/Default"));
             lRend.startColor = c;
             lRend.endColor = c;
         } else {
-
+            Debug.Log("SenseMetal is false.");
         }
     }
 }
