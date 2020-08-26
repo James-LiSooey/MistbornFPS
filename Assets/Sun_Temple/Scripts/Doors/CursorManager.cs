533b51e5e1a6fa10d0ee86a3cc63bcfdcfe03463
kmm11411@gmail.com <kmm11411@gmail.com>
2020-08-25 20:56:53 -0400

    imported Sun Temple assets

---
+++ Assets/Sun_Temple/Scripts/Doors/CursorManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace SunTemple{
+	
+	public class CursorManager : MonoBehaviour {
+
+		public static CursorManager instance;
+
+		public Sprite defaultCursor;
+		public Sprite lockedCursor;
+		public Sprite doorCursor;
+
+		private Image img;
+
+
+		void Awake () {
+			instance = this;
+			img = GetComponent<Image> ();						
+		}
+
+
+		public void SetCursorToLocked(){
+			img.sprite = lockedCursor;
+			
+		}
+
+		public void SetCursorToDoor(){
+			img.sprite = doorCursor;
+		}
+
+		public void SetCursorToDefault(){
+			img.sprite = defaultCursor; 
+		}
+		
+
+	}
+}
